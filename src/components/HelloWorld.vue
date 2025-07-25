02ec7ed88ba03ecbed90a88a08e2ad61475671e2
xpzz521 <52185806+xpzz521@users.noreply.github.com>
2025-07-25 11:40:46 +0800

    Add HelloWorld Vue component

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      Welcome to the Document UI Demo
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
